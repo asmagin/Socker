14cc1bb096bc299568a2a29cd6c1b7b70a18be77
Alexander Smagin <alexander.v.smagin@gmail.com>
2017-03-31 12:25:24 -0400

    Finalize jenkins slaves setup, migrate to 2.46.1

--- Build.ps1
+++ Build.ps1
@@ -1,30 +1,38 @@
 $ErrorActionPreference = "STOP"
 
-$version = "8.2 rev. 161221"
-$shortVer = ($version -replace " ", "") -replace "rev", "";
+$SC_VERSION = "8.2 rev. 161221"
+$SC_VERSION_SHORT = ($SC_VERSION -replace " ", "") -replace "rev", "";
 
-# unzip sitecore and put files in required folders
-Remove-item ".\dependencies\Sitecore $version" -Force -Recurse -ErrorAction SilentlyContinue
-Remove-item ".\images\sitecore-$shortVer\Sitecore" -Force -Recurse -ErrorAction SilentlyContinue
-Remove-item ".\images\sitecore-$shortVer-db\Sitecore" -Force -Recurse -ErrorAction SilentlyContinue
+$JENKINS_VERSION = "2.46.1"
 
-& ".\tools\7z.exe" "x" ".\dependencies\Sitecore $version.zip" "-aoa" "-o.\dependencies"
+# # unzip sitecore and put files in required folders
+# Remove-item ".\dependencies\Sitecore $SC_VERSION" -Force -Recurse -ErrorAction SilentlyContinue
+# Remove-item ".\images\sitecore-$SC_VERSION_SHORT\Sitecore" -Force -Recurse -ErrorAction SilentlyContinue
+# Remove-item ".\images\sitecore-$SC_VERSION_SHORT-db\Sitecore" -Force -Recurse -ErrorAction SilentlyContinue
 
-## Move folders close to images
-# Move website into sitecore base image
-New-Item -path ".\images\sitecore-$shortVer\Sitecore" -ItemType Directory -Force
-New-Item -path ".\images\sitecore-$shortVer\Overrides" -ItemType Directory -Force
-Move-Item ".\dependencies\Sitecore $version\Website" ".\images\sitecore-$shortVer\Sitecore" -Force
-Move-Item ".\dependencies\Sitecore $version\Data"    ".\images\sitecore-$shortVer\Sitecore" -Force
+# & ".\tools\7z.exe" "x" ".\dependencies\Sitecore $SC_VERSION.zip" "-aoa" "-o.\dependencies"
 
-# Move database files
-New-Item -path ".\images\sitecore-$shortVer-db\Sitecore" -ItemType Directory -Force
-Move-Item ".\dependencies\Sitecore $version\Databases" ".\images\sitecore-$shortVer-db\Sitecore" -Force
+# ## Move folders close to images
+# # Move website into sitecore base image
+# New-Item -path ".\images\sitecore-$SC_VERSION_SHORT\Sitecore" -ItemType Directory -Force
+# New-Item -path ".\images\sitecore-$SC_VERSION_SHORT\Overrides" -ItemType Directory -Force
+# Move-Item ".\dependencies\Sitecore $SC_VERSION\Website" ".\images\sitecore-$SC_VERSION_SHORT\Sitecore" -Force
+# Move-Item ".\dependencies\Sitecore $SC_VERSION\Data"    ".\images\sitecore-$SC_VERSION_SHORT\Sitecore" -Force
 
-docker build -t epam/sitecore:8.2.161221 .\images\sitecore-$shortVer
-docker build -t epam/sitecore-db:8.2.161221 .\images\sitecore-$shortVer-db
-docker build -t epam/jenkins:2.32.2 .\images\jenkins-2.32.2
-docker build -t epam/sitecore-jenkins-master .\images\sitecore-jenkins-master
+# # Move database files
+# New-Item -path ".\images\sitecore-$SC_VERSION_SHORT-db\Sitecore" -ItemType Directory -Force
+# Move-Item ".\dependencies\Sitecore $SC_VERSION\Databases" ".\images\sitecore-$SC_VERSION_SHORT-db\Sitecore" -Force
+
+docker rm $(docker ps -a -q)
+docker rmi $(docker images -f dangling=true -q)
+ 
+# docker build -t epam/sitecore:$SC_VERSION_SHORT            .\images\sitecore-$SC_VERSION_SHORT
+# docker build -t epam/sitecore-db:$SC_VERSION_SHORT         .\images\sitecore-$SC_VERSION_SHORT-db
+docker build -t epam/jenkins:$JENKINS_VERSION              .\images\jenkins-$JENKINS_VERSION
+docker build -t epam/jenkins-slave:$JENKINS_VERSION        .\images\jenkins-$JENKINS_VERSION-slave
+
+docker build -t epam/sitecore-jenkins-master               .\images\sitecore-jenkins-master
+docker build -t epam/sitecore-jenkins-slave                .\images\sitecore-jenkins-slave
 # docker build -t sitecore-iis .\images\sitecore-iis
 # docker build -t sitecore:8.2.161221 .\images\sitecore-82rev161221
 # docker build -t traefik:win .\images\traefik-win
