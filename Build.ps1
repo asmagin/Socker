f34e7e0b9485c04a625172db221a44156d42c610
Alexander Smagin <alexander.v.smagin@gmail.com>
2017-04-01 16:43:24 -0400

    Refactor Dockerfile scripts

--- Build.ps1
+++ Build.ps1
@@ -28,11 +28,11 @@ docker rmi $(docker images -f dangling=true -q)
  
 # docker build -t epam/sitecore:$SC_VERSION_SHORT            .\images\sitecore-$SC_VERSION_SHORT
 # docker build -t epam/sitecore-db:$SC_VERSION_SHORT         .\images\sitecore-$SC_VERSION_SHORT-db
-docker build -t epam/jenkins:$JENKINS_VERSION              .\images\jenkins-$JENKINS_VERSION
-docker build -t epam/jenkins-slave:$JENKINS_VERSION        .\images\jenkins-$JENKINS_VERSION-slave
+# docker build -t asmagin/jenkins-on-windowsservercore:$JENKINS_VERSION              .\images\jenkins-$JENKINS_VERSION
+# docker build -t asmagin/jenkins-on-windowsservercore-slave:$JENKINS_VERSION        .\images\jenkins-$JENKINS_VERSION-slave
 
 docker build -t epam/sitecore-jenkins-master               .\images\sitecore-jenkins-master
-docker build -t epam/sitecore-jenkins-slave                .\images\sitecore-jenkins-slave
+# docker build -t epam/sitecore-jenkins-slave                .\images\sitecore-jenkins-slave
 # docker build -t sitecore-iis .\images\sitecore-iis
 # docker build -t sitecore:8.2.161221 .\images\sitecore-82rev161221
 # docker build -t traefik:win .\images\traefik-win
